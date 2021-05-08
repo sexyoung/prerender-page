53c945eda2b46b7be8f3ae4b53ee559be446e165
寫詩羊 <Habuche@gmail.com>
2021-05-08 21:24:58 +0800

    initial

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import App from './App';
+
+import { hydrate, render } from "react-dom";
+
+const rootElement = document.getElementById("root");
+if (rootElement!.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
